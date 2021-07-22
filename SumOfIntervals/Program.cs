a6fe97d8087b0adc8b15bc42eeb306b75331c8d1
Ihor Volokhovych <Ihor.volokhovych@pm.bet>
2021-07-23 00:08:33 +0300

    + kata

--- SumOfIntervals/Program.cs
+++ SumOfIntervals/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Interval = System.ValueTuple<int, int>;
 
@@ -6,22 +7,25 @@ namespace SumOfIntervals
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(SumIntervals(new Interval[] { (-1, 4), (-5, -3) }));
+            Console.WriteLine(SumIntervals(new[] { (1, 5), (10, 20), (1, 6), (16, 19), (5, 11) }));
             
         }
         
         public static int SumIntervals((int, int)[] intervals)
         {
+            var array = new List<int>();
             for (var i = 0; i < intervals.Length; i++)
             {
-                if (intervals[i].Item1 < 0 && intervals[i].Item2 < 0)
+                for(var k = intervals[i].Item1; k < intervals[i].Item2; k++)
                 {
-                    if(intervals[i+1].Item1< intervals[i].Item1)
+                  array.Add(k);
                 }
             }
+            return array.Distinct().Count();
         }
     }
 }
